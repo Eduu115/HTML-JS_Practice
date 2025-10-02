dd4e0713df0b81135b9af9183ab37db3b823f093
Eduu115 <serranotrenado.e@gmail.com>
2025-10-02 11:13:38 +0200

    finalizado estetica y funcionalmente el ejercicio 1

--- Calificacion_Alfabetica/app.js
+++ Calificacion_Alfabetica/app.js
@@ -2,35 +2,53 @@
 
 const resultados = ["Muy deficiente", "Insuficiente", "Bien", "Notable", "Sobresaliente"];
 
-document.addEventListener("DOMContentLoaded", () => {
-    // obtenemos elementos del documento
-    let caja_resultados = document.getElementById('resultado');
 
-    let formulario = document.getElementById('form');
+// obtenemos elementos del documento
+let caja_resultados = document.getElementById('resultado');
+let boton_reiniciar = document.getElementById('reload');
 
-    formulario.addEventListener('submit', (e)=>{
-        e.preventDefault();
+let formulario = document.getElementById('form');
+
+formulario.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    let nota = document.getElementById("nota").value;
+    nota = parseInt(nota);
+
+    if (nota > 10 || nota < 0) {
+        alert("Por favor, introduce una nota válida entre 0 y 10.");
+        return;
+    }
+    if (nota == null || isNaN(nota)) {
+        alert("Por favor, introduce una nota.");
+        return;
+    }
+
+
+    console.log(nota)
+
+    let resultadoFinal = null;
+
+    if (nota == 0) {
+        resultadoFinal = resultados[0];
+    } else if (nota > 0 && nota < 5) {
+        resultadoFinal = resultados[1];
+    } else if (nota == 5 || nota == 6) {
+        resultadoFinal = resultados[2];
+    } else if (nota == 7 || nota == 8) {
+        resultadoFinal = resultados[3]
+    } else if (nota == 9 || nota == 10) {
+        resultadoFinal = resultados[4]
+    }
+
+    console.log(resultadoFinal);
+
+    caja_resultados.innerHTML=`
+        ${resultadoFinal}
+    `;
         
-        let nota = document.getElementById("nota").value;
+});
 
-        let resultadoFinal= null;
-
-        if (nota == 0) {
-            resultadoFinal = resultados[0];
-        } else if (nota in (1,4)) {
-            resultadoFinal = resultados[1];
-        } else if (nota in (5,6)) {
-            resultadoFinal = resultados[2];
-        } else if (nota in (7,8)) {
-            resultadoFinal = resultados[3]
-        } else if (nota in (9,10)) {
-            resultadoFinal = resultados[4]
-        }
-
-        caja_resultados.innerHTML=`
-            ${resultadoFinal}
-        `;
-         
-    });
-
+boton_reiniciar.addEventListener('click', () => {
+    window.location.reload();
 });
